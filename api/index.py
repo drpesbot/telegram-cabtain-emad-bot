4604cb8d11fd9a179eb8eee3b92f7b58c19256e8
drpesbot <dr.gym9999@gmail.com>
2025-09-10 07:09:27 -0400

    Update index.py for python-telegram-bot v20.x.x compatibility

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
 from telegram import Update, Bot
-from telegram.ext import Dispatcher, MessageHandler, filters, CommandHandler
+from telegram.ext import Application, MessageHandler, filters, CommandHandler
 
 TOKEN = os.environ.get("TOKEN")  # التوكن هيتحدد في Vercel
 
@@ -16,28 +16,24 @@ class TelegramWebhook(BaseModel):
     message: Optional[dict]
 
 def start(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="اضغط على الرابط الذى بالاسفل للوصول إلى ستور كابتن عماد \nهذا هو الرابط تفضل بالضغط عليه ⬇️⬇️\nhttps://t.me/pes224"
+    update.message.reply_text(
+        "اضغط على الرابط الذى بالاسفل للوصول إلى ستور كابتن عماد \nهذا هو الرابط تفضل بالضغط عليه ⬇️⬇️\nhttps://t.me/pes224"
     )
 
 def echo(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="اضغط على الرابط الذى بالاسفل للوصول إلى ستور كابتن عماد \nهذا هو الرابط تفضل بالضغط عليه ⬇️⬇️\nhttps://t.me/pes224"
+    update.message.reply_text(
+        "اضغط على الرابط الذى بالاسفل للوصول إلى ستور كابتن عماد \nهذا هو الرابط تفضل بالضغط عليه ⬇️⬇️\nhttps://t.me/pes224"
     )
 
-def register_handlers(dispatcher):
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
 @app.post("/webhook")
-def webhook(webhook_data: TelegramWebhook):
+async def webhook(webhook_data: dict):
     bot = Bot(token=TOKEN)
-    update = Update.de_json(webhook_data.__dict__, bot)
-    dispatcher = Dispatcher(bot, None, workers=4)
-    register_handlers(dispatcher)
-    dispatcher.process_update(update)
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    update = Update.de_json(webhook_data, bot)
+    await application.process_update(update)
     return {"message": "ok"}
 
 @app.get("/")
